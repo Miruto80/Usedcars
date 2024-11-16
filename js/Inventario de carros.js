328d0e3f43448efc6e0bea95671a6363b906e185
Miruto80 <miguelfernando8012@gmail.com>
2024-11-16 14:21:30 -0400

    Arreglandoo

--- js/Inventario de carros.js
+++ js/Inventario de carros.js
@@ -91,13 +91,16 @@ $("#name").on("keyup", function () {
 // Función de validación del envío
 function validarenvio() {
     if (validarkeyup(/^[0-9]{10,11}$/, $("#phone"), $("#sphone"), "The format must be between 10 to 11 digits") == 0) {
+        alert("Error in phone");
         return false;
     }
     if (validarkeyup(/^[A-Za-z_0-9\u00f1\u00d1\u00E0-\u00FC-]{3,20}[@]{1}[A-Za-z0-9]{3,8}[.]{1}[A-Za-z]{3}$/, 
         $("#email"), $("#semail"), "The format must be like name@server.com") == 0) {
+            alert("Error in email");
         return false;
     }
     if (validarkeyup(/^[A-Za-z\b\s\u00f1\u00d1\u00E0-\u00FC]{3,30}$/, $("#name"), $("#sname"), "Only letters between 3 and 30 characters") == 0) {
+        alert("Error in name");
         return false;
     }
     return true;
