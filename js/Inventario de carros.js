5220ae9fb678d256261e0cbaa5981efa1d47f071
Miruto80 <miguelfernando8012@gmail.com>
2025-02-19 16:18:12 -0400

    Arreglando

--- js/Inventario de carros.js
+++ js/Inventario de carros.js
@@ -63,22 +63,21 @@ function nextImage(button) {
 }
 
 // Validaciones para los campos de nombre, teléfono y email
-$("#contact").on("keypress", function (e) {
+ $("#phone").on("keypress", function (e) {
+    validarkeypress(/^[0-9-\b]*$/, e);
+});
+
+$("#phone").on("keyup", function () {
+    validarkeyup(/^[0-9]{10,11}$/, $(this), $("#sphone"), "The format must be between 10 to 11 digits");
+});
+
+$("#email").on("keypress", function (e) {
     validarkeypress(/^[A-Za-z@_.0-9\b\u00f1\u00d1\u00E0-\u00FC-]*$/, e);
 });
 
-$("#contact").on("keyup", function () {
-    let valor = $(this).val();
-    let emailRegex = /^[A-Za-z_0-9\u00f1\u00d1\u00E0-\u00FC-]{3,20}[@]{1}[A-Za-z0-9]{3,8}[.]{1}[A-Za-z]{3}$/;
-    let phoneRegex = /^[0-9]{10,11}$/;
-    
-    if (emailRegex.test(valor) || phoneRegex.test(valor)) {
-        $("#scontact").text("");
-        return 1;
-    } else {
-        $("#scontact").text("Enter a valid email or phone");
-        return 0;
-    }
+$("#email").on("keyup", function () {
+    validarkeyup(/^[A-Za-z_0-9\u00f1\u00d1\u00E0-\u00FC-]{3,20}[@]{1}[A-Za-z0-9]{3,8}[.]{1}[A-Za-z]{3}$/,
+        $(this), $("#semail"), "The format must be like name@server.com");
 });
 
 $("#name").on("keypress", function (e) {
@@ -91,16 +90,19 @@ validarkeyup(/^[A-Za-z\b\s\u00f1\u00d1\u00E0-\u00FC]{3,30}$/, $(this), $("#sname
 
 // Función de validación del envío
 function validarenvio() {
-if ($("#contact").val().length === 0 || validarkeyup(/^[A-Za-z_0-9\u00f1\u00d1\u00E0-\u00FC-]{3,20}[@]{1}[A-Za-z0-9]{3,8}[.]{1}[A-Za-z]{3}$/, 
-    $("#contact"), $("#scontact"), "Enter a valid email or phone") == 0 && 
-    validarkeyup(/^[0-9]{10,11}$/, $("#contact"), $("#scontact"), "Enter a valid email or phone") == 0) {
-    alert("Error in email or phone");
-    return false;
-}
-if (validarkeyup(/^[A-Za-z\b\s\u00f1\u00d1\u00E0-\u00FC]{3,30}$/, $("#name"), $("#sname"), "Only letters between 3 and 30 characters") == 0) {
-alert("Error in name");
-return false;
-}
+    if (validarkeyup(/^[0-9]{10,11}$/, $("#phone"), $("#sphone"), "The format must be between 10 to 11 digits") == 0) {
+        alert("Error in phone");
+        return false;
+    }
+    if (validarkeyup(/^[A-Za-z_0-9\u00f1\u00d1\u00E0-\u00FC-]{3,20}[@]{1}[A-Za-z0-9]{3,8}[.]{1}[A-Za-z]{3}$/, 
+        $("#email"), $("#semail"), "The format must be like name@server.com") == 0) {
+            alert("Error in email");
+        return false;
+    }
+     if (validarkeyup(/^[A-Za-z\b\s\u00f1\u00d1\u00E0-\u00FC]{3,30}$/, $("#name"), $("#sname"), "Only letters between 3 and 30 characters") == 0) {
+     alert("Error in name");
+      return false;
+    }
 return true;
 }
 
